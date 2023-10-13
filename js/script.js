490733a4cfdfcf75d84d1025fdf82afd9414197f
jangddoll <xng0312@gmail.com>
2023-10-13 15:48:29 +0900

    CSS 수정

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@ const searchInputEl = searchEl.querySelector("input");
 searchEl.addEventListener("click", function () {
     searchInputEl.focus();
 })
+
 searchInputEl.addEventListener("focus", function () {
     searchEl.classList.add("focused");
     searchInputEl.setAttribute("placeholder", "통합검색");
 })
+
 searchInputEl.addEventListener("blur", function () {
     searchEl.classList.remove("focused");
     searchInputEl.setAttribute("placeholder", "");
